832f38b65fa0f1c92f9187c0c74cba66aa4e70b6
Ibrahim <ibrahimbsc8@gmail.com>
2022-01-30 11:57:35 -0800

    fix errors

--- models/user.py
+++ models/user.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Represent a User.
-
     Attributes:
         email (str): The email of the user.
         password (str): The password of the user.
